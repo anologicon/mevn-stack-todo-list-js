453522dbec1c34cba14188afb94fcc560c0fde7d
pauloraimundi2009 <pauloraimundi2009@unifebe.edu.br>
2018-11-01 10:01:50 -0400

    mongo atlas

--- app/Config.js
+++ app/Config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    DB: 'mongodb://localhost:27017/todos',
+    DB: 'mongodb://anologicon:MBRmMfcvNvcSARM9@todolist-shard-00-00-wmz4g.mongodb.net:27017,todolist-shard-00-01-wmz4g.mongodb.net:27017,todolist-shard-00-02-wmz4g.mongodb.net:27017/test?ssl=true&replicaSet=todolist-shard-0&authSource=admin&retryWrites=true',
     APP_PORT: 7000,
 }
