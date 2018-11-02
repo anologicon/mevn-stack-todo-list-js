23a18da1e411be628472e7832eed41a0c6b759bc
Anologicon <pauloraimundi2009@gmail.com>
2018-11-01 22:19:34 -0300

    Cors please

--- index.js
+++ index.js
@@ -41,11 +41,10 @@ var todoRoutes = require('./app/Routes');
 app.use('/api', todoRoutes);
 
 app.use(function(req, res, next){
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE'); // If needed
-    res.setHeader('Access-Control-Allow-Credentials', true); // If needed
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.set('Access-Control-Allow-Origin', '*');
+    res.set('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE'); // If needed
+    res.set('Access-Control-Allow-Credentials', true); // If needed
+    res.set("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     // Pass to next layer of middleware
     next();
 });
