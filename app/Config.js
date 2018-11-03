3dcc2dc0fffd1b9e5ca672bdd5598f22a7f33dfd
Anologicon <pauloraimundi2009@gmail.com>
2018-11-03 15:19:59 -0300

    Log envaroment

--- app/Config.js
+++ app/Config.js
@@ -1,4 +1,17 @@
+var os = require('os');
+var database_uri;
+
+console.log(os.hostname());
+
+if (os.hostname().indexOf("local") > -1) {
+	database_uri = 'mongodb://localhost:27017/todos';
+	console.log('Banco local');
+} else {
+	database_uri = 'mongodb://anologicon:MBRmMfcvNvcSARM9@todolist-shard-00-00-wmz4g.mongodb.net:27017,todolist-shard-00-01-wmz4g.mongodb.net:27017,todolist-shard-00-02-wmz4g.mongodb.net:27017/test?ssl=true&replicaSet=todolist-shard-0&authSource=admin&retryWrites=true';
+	console.log('Banco atlas');
+}
+
 module.exports = {
-    DB: 'mongodb://anologicon:MBRmMfcvNvcSARM9@todolist-shard-00-00-wmz4g.mongodb.net:27017,todolist-shard-00-01-wmz4g.mongodb.net:27017,todolist-shard-00-02-wmz4g.mongodb.net:27017/test?ssl=true&replicaSet=todolist-shard-0&authSource=admin&retryWrites=true',
+    DB: database_uri,
     APP_PORT: 7000,
 }
